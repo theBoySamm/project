a5ce11e19f87b6525bb6593f3ccb2dd6736a4e7e
theBoySamm <onyemasamuel96@gmail.com>
2021-07-17 01:48:41 +0100

    init

---
+++ main.js
@@ -0,0 +1,6 @@
+// window.addEventListener("scroll", function () {
+//     let navBar = document.querySelector("nav");
+//     let windowPosition = window.scrollY > 0;
+
+//     navBar.classList.toggle("scroll-active", windowPosition)
+// })
